c2d6dc1d73535e92beb5f77571cf7a85ba85ab1b
Carlos Hurtado <hjuan3721@gmail.com>
2026-08-16 23:47:04 -0600

    Implement browser startup test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+
+    @Test
+    void abrirNavegador() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Título: " + driver.getTitle());
+
+        driver.quit();
+    }
 }
