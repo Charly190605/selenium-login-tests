3e43f58364d16cf1b263ec02b82acf12a9e800f8
Carlos Hurtado <hjuan3721@gmail.com>
2026-08-16 23:58:08 -0600

    Add invalid username login test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,7 +11,10 @@ public class LoginTest {
     @Test
     void loginCorrecto() {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-features=PasswordLeakDetection");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://the-internet.herokuapp.com/login");
 
@@ -53,6 +57,32 @@ public class LoginTest {
 
         assertEquals(
                 "Your password is invalid!\n×",
+                mensaje
+        );
+
+        driver.quit();
+    }
+
+    @Test
+    void loginUsuarioIncorrecto() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username"))
+                .sendKeys("usuarioIncorrecto");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.cssSelector("button[type='submit']"))
+                .click();
+
+        String mensaje = driver.findElement(By.id("flash")).getText();
+
+        assertEquals(
+                "Your username is invalid!\n×",
                 mensaje
         );
 
